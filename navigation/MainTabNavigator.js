7bc389952995b107e45c461ff1a01681bf3bb179
thienth32 <thienth32@gmail.com>
2019-05-24 12:47:03 +0700

    init

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  ngabeo: SettingsScreen
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const AddStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+AddStack.navigationOptions = {
+  tabBarLabel: 'Add new',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-add' : 'md-add'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  AddStack
+});
